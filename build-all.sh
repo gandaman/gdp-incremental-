818b0f9a5fabf611f12ecb093658569f6fde98e0
Gunnar Andersson <gandersson@genivi.org>
2017-05-28 15:32:20 +0200

    fixing

--- build-all.sh
+++ build-all.sh
@@ -3,26 +3,21 @@
 # (C) 2017 Gunnar Andersson
 # LICENSE: MPLv2
 
-usage() {
-  local name=$(basename "$0")
-  echo "Usage: $name <target> <git-ref>"
-  echo "Ref can be a commit-hash, branch, tag, etc."
-  echo "target is a valid build target (hardware) for GDP"
-  exit 1
-}
-
-# This is some trickery, but quite useful
-deref() { eval echo \$$1 ; }
-
 D=$(dirname "$0")
 cd "$D"
 MYDIR="$PWD"
 
-for d in base targets/*/{base,source,build} ; do
+d=docker/base
+cd "$d"
+echo "------------- $d -----------"
+docker build -t gdpbuild/base .
+cd "$MYDIR"
+
+for d in docker/targets/*/{base,source,build} ; do
   cd "$d"
+  tag="gdpbuild/$(basename $(readlink -f $PWD/..))_$(basename $PWD)"
   echo "------------- $d -----------"
-  echo "+ docker build -t gdpbuild/$d"
-  docker build -t gdpbuild/$d .
+  docker build -t $tag .
   echo
   cd "$MYDIR"
 done
